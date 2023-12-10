8937678bd92286e4fd645103464263bf881dc51d
fp563 <matsu.yuri@gmail.com>
2023-12-11 00:32:10 +0900

    Add invoice edit page and update actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { z } from 'zod';
-import {sql} from '@vercel/postgres'
+import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
@@ -9,19 +9,19 @@ const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
-  status: z.enum(['pending','paid']),
+  status: z.enum(['pending', 'paid']),
   date: z.string(),
-})
+});
 
-const CreateInvoice = FormSchema.omit({id:true,date:true})
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData:FormData) {
-  const {customerId,amount,status} = CreateInvoice.parse({
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  const amountInCents = amount*100;
+  const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
   await sql`
@@ -29,5 +29,26 @@ export async function createInvoice(formData:FormData) {
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices')
+  redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
